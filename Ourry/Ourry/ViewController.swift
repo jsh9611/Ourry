2bcafd1a1aa76ec51e676208c2edf9ac30e1143c
jsh9611 <jangsh9611@naver.com>
2023-11-28 21:58:36 +0900

    add: start of xcode project
    remove storyboard & set supported version and language

---
+++ Ourry/Ourry/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ourry
+//
+//  Created by SeongHoon Jang on 2023/11/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
