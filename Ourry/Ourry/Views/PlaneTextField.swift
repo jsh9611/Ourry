0631915b22b94717b5c0d213c63a933eee74c4bf
jsh9611 <jangsh9611@naver.com>
2023-12-04 19:56:51 +0900

    add: custom textfield components

---
+++ Ourry/Ourry/Views/PlaneTextField.swift
@@ -0,0 +1,23 @@
+//
+//  PlaneTextField.swift
+//  Ourry
+//
+//  Created by SeongHoon Jang on 2023/11/30.
+//
+
+import UIKit
+
+class PlaneTextField: UITextField {
+
+    init(placeholder: String) {
+        super.init(frame: .zero)
+        
+        self.placeholder = placeholder
+        
+        self.borderStyle = .roundedRect
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
