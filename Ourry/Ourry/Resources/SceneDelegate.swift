7d89fac66ac52191430b12d37405ebc98678b101
jsh9611 <jangsh9611@naver.com>
2024-02-17 21:21:59 +0900

    refactor: delete unused codes and allow all http requests

--- Ourry/Ourry/Resources/SceneDelegate.swift
+++ Ourry/Ourry/Resources/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
-        let tempViewController = MainViewController()//MainViewController()//TempViewController()
+        let tempViewController = TempViewController()
         let navigationController = UINavigationController(rootViewController: tempViewController)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
